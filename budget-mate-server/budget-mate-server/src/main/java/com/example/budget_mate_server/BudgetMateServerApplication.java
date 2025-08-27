bc3f2aae7aaa6a0eab2d36057d29c32c6e8e9990
kavindasj-at-832266882009 <kavindasj@cf.lk>
2025-08-27 10:52:57 +0530

    first commit

---
+++ budget-mate-server/budget-mate-server/src/main/java/com/example/budget_mate_server/BudgetMateServerApplication.java
@@ -0,0 +1,13 @@
+package com.example.budget_mate_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BudgetMateServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BudgetMateServerApplication.class, args);
+	}
+
+}
---
+++ budget-mate-server/budget-mate-server/src/test/java/com/example/budget_mate_server/BudgetMateServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.budget_mate_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BudgetMateServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
