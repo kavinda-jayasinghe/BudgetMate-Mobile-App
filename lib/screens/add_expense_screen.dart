a0e0ac772f01bc4720180192d7a0556558372fde
kavinda-jayasinghe <kavindajayasinghequn@gmail.com>
2025-10-11 21:37:31 +0530

    add income

--- lib/screens/add_expense_screen.dart
+++ lib/screens/add_expense_screen.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:cloud_firestore/cloud_firestore.dart';  // For Firestore
-import 'package:intl/intl.dart';  // For formatting date
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:intl/intl.dart';
 
 class AddExpenseScreen extends StatefulWidget {
   const AddExpenseScreen({super.key});
@@ -10,38 +10,44 @@ class AddExpenseScreen extends StatefulWidget {
 }
 
 class _AddExpenseScreenState extends State<AddExpenseScreen> {
-  final _formKey = GlobalKey<FormState>();
+  final _expenseFormKey = GlobalKey<FormState>(); // Key for Expense form
+  final _incomeFormKey = GlobalKey<FormState>(); // Key for Income form
   final _amountController = TextEditingController();
   final _categoryController = TextEditingController();
   final _noteController = TextEditingController();
   DateTime _selectedDate = DateTime.now();
 
-  // Method to save the expense to Firestore
-  Future<void> _saveExpense() async {
-    if (_formKey.currentState!.validate()) {
+  bool _isExpense = true;
+
+  Future<void> _saveExpenseIncome(GlobalKey<FormState> formKey) async {
+    if (formKey.currentState!.validate()) {
       final amount = double.parse(_amountController.text);
       final category = _categoryController.text;
       final note = _noteController.text;
+      final type = _isExpense ? 'expense' : 'income';
 
-      // Save expense to Firestore
-      await FirebaseFirestore.instance.collection('expenses').add({
-        'amount': amount,
-        'category': category,
-        'note': note,
-        'date': Timestamp.fromDate(_selectedDate),  // Save date as Firestore Timestamp
-      });
+      try {
+        await FirebaseFirestore.instance.collection('transactions').add({
+          'amount': amount,
+          'category': category,
+          'note': note,
+          'date': Timestamp.fromDate(_selectedDate),
+          'type': type,
+        });
 
-      // Show success message
-      ScaffoldMessenger.of(context).showSnackBar(
-        const SnackBar(content: Text('Expense added successfully!')),
-      );
+        ScaffoldMessenger.of(context).showSnackBar(
+          const SnackBar(content: Text('Transaction added successfully!')),
+        );
 
-      // Close the screen after saving the expense
-      Navigator.pop(context);
+        Navigator.pop(context);
+      } catch (e) {
+        ScaffoldMessenger.of(context).showSnackBar(
+          SnackBar(content: Text('Error: $e')),
+        );
+      }
     }
   }
 
-  // Method to open date picker
   Future<void> _selectDate(BuildContext context) async {
     final DateTime? picked = await showDatePicker(
       context: context,
@@ -57,70 +63,96 @@ class _AddExpenseScreenState extends State<AddExpenseScreen> {
   }
 
   @override
+  void dispose() {
+    _amountController.dispose();
+    _categoryController.dispose();
+    _noteController.dispose();
+    super.dispose();
+  }
+
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Add Expense'),
+        title: const Text('Add Expense/Income'),
       ),
       body: Padding(
         padding: const EdgeInsets.all(16.0),
-        child: Form(
-          key: _formKey,
-          child: ListView(
+        child: DefaultTabController(
+          length: 2,
+          child: Column(
             children: [
-              // Amount field
-              TextFormField(
-                controller: _amountController,
-                keyboardType: TextInputType.number,
-                decoration: const InputDecoration(labelText: 'Amount'),
-                validator: (value) {
-                  if (value == null || value.isEmpty) {
-                    return 'Please enter an amount';
-                  }
-                  if (double.tryParse(value) == null) {
-                    return 'Enter a valid number';
-                  }
-                  return null;
-                },
+              const TabBar(
+                tabs: [
+                  Tab(text: 'Expense'),
+                  Tab(text: 'Income'),
+                ],
               ),
-              const SizedBox(height: 16),
-
-              // Category field
-              TextFormField(
-                controller: _categoryController,
-                decoration: const InputDecoration(labelText: 'Category'),
-                validator: (value) {
-                  if (value == null || value.isEmpty) {
-                    return 'Please enter a category';
-                  }
-                  return null;
-                },
-              ),
-              const SizedBox(height: 16),
-
-              // Note field
-              TextFormField(
-                controller: _noteController,
-                decoration: const InputDecoration(labelText: 'Note'),
-              ),
-              const SizedBox(height: 16),
-
-              // Date picker
-              ListTile(
-                title: Text('Date: ${DateFormat.yMMMMd().format(_selectedDate)}'),
-                trailing: const Icon(Icons.calendar_today),
-                onTap: () => _selectDate(context),
-              ),
-              const SizedBox(height: 16),
-
-              // Submit Button
-              ElevatedButton(
-                onPressed: _saveExpense,
-                child: const Text('Save Expense'),
+              Expanded(
+                child: TabBarView(
+                  children: [
+                    _buildTransactionForm(isExpense: true, formKey: _expenseFormKey),
+                    _buildTransactionForm(isExpense: false, formKey: _incomeFormKey),
+                  ],
+                ),
               ),
             ],
           ),
         ),
+      ),
+    );
+  }
+
+  Widget _buildTransactionForm({required bool isExpense, required GlobalKey<FormState> formKey}) {
+    setState(() {
+      _isExpense = isExpense;
+    });
+    return Form(
+      key: formKey,
+      child: ListView(
+        children: [
+          TextFormField(
+            controller: _amountController,
+            keyboardType: TextInputType.number,
+            decoration: const InputDecoration(labelText: 'Amount'),
+            validator: (value) {
+              if (value == null || value.isEmpty) {
+                return 'Please enter an amount';
+              }
+              if (double.tryParse(value) == null) {
+                return 'Enter a valid number';
+              }
+              return null;
+            },
+          ),
+          const SizedBox(height: 16),
+          TextFormField(
+            controller: _categoryController,
+            decoration: const InputDecoration(labelText: 'Category'),
+            validator: (value) {
+              if (value == null || value.isEmpty) {
+                return 'Please enter a category';
+              }
+              return null;
+            },
+          ),
+          const SizedBox(height: 16),
+          TextFormField(
+            controller: _noteController,
+            decoration: const InputDecoration(labelText: 'Note'),
+          ),
+          const SizedBox(height: 16),
+          ListTile(
+            title: Text('Date: ${DateFormat.yMMMMd().format(_selectedDate)}'),
+            trailing: const Icon(Icons.calendar_today),
+            onTap: () => _selectDate(context),
+          ),
+          const SizedBox(height: 16),
+          ElevatedButton(
+            onPressed: () => _saveExpenseIncome(formKey),
+            child: Text(isExpense ? 'Save Expense' : 'Save Income'),
+          ),
+        ],
       ),
     );
   }
